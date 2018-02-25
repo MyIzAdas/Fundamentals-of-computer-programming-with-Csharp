19401b3d8a38f1424560af44cc5558637383ae57
Adas <Adas@URGODS-PC>
2018-02-25 17:03:30 +0000

    Ex1

---
+++ Chapter1 Exercise1/Chapter1 Exercise1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort a = 52130;
+            sbyte b =-115;
+            int c = 4825932;
+            byte d = 97;
+            short e = -10000;
+            short f = 20000;
+            byte g = 224;
+             int  h = 970700000;
+            byte i = 112;
+            sbyte j = -44;
+            int k =-1000000;
+            short l = 1990;
+            long m = 123456789123456789;
+            decimal n = 34.567839023m;
+        }
+    }
+}
