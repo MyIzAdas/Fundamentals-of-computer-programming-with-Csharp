21dad1fc554b51dd68454066a538e68fdb212be8
Adas <Adas@URGODS-PC>
2018-03-04 15:26:30 +0000

    ex1

---
+++ Chapter7/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            for(int i=0;i<20;i++)
+            {
+                array[i] = i*5;
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
