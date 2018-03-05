71b3bac839c40bd076d6eb00a03970fdf4ce27ee
Adas <Adas@URGODS-PC>
2018-03-05 00:08:25 +0000

    More exercises

---
+++ Chapter7/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 2 char arrays to compare their values lexicographically");
+            Console.Write("Please enter size of the first array: ");
+
+            //Reading first array
+            int firstArraySize = int.Parse(Console.ReadLine());
+            char[] firstArray = new char[firstArraySize];
+            Console.WriteLine("Please enter first char array.");
+            char firstChar = 'a';
+            for (int i=0; i<firstArraySize; i++)
+            {
+                firstArray[i] = char.Parse(Console.ReadLine());
+            }
+
+            //Reading second array
+            Console.Write("Please enter size of second array");
+            int secondArraysize = int.Parse(Console.ReadLine());
+            char[] secondArray = new char[secondArraysize];
+            Console.WriteLine("Please enter second char array");
+            for (int i = 0; i < secondArraysize; i++)
+            {
+                secondArray[i] = char.Parse(Console.ReadLine());
+            }
+
+            //Comparing arrays
+            for(int i=0;i<firstArraySize;i++)
+            {
+                for(int n=0;n<secondArraysize;n++)
+                {
+                    if(firstArray[i]>secondArray[n])
+                    {
+                        firstChar = secondArray[n];
+                    }
+                    else
+                    {
+                        firstChar = firstArray[i];
+                    }
+                }
+            }
+            Console.WriteLine("First char is: " + firstChar);
+        }
+    }
+}
---
+++ Chapter7/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timesTrue = 0;
+            int num = 0;
+            int ttaInex = 0;
+            Console.WriteLine("This program finds maximal sequence of consecutive equal elements within array.");
+            Console.Write("Please enter size of array: ");
+
+            //Reading array
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int[] timesTrueArray = new int [n];
+            int[] numArray = new int[n];
+
+            Console.WriteLine("Please enter array of int.");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //finding consecutive equal elements within array
+            for(int i=0;i<n;i++)
+            {
+                int a = i;
+                if(i==n-1)
+                {
+                    a--;
+                }
+                if(array[a]==array[a+1])
+                {
+                    timesTrue++;
+                    num = array[a];
+                }
+                else if(timesTrue>0)
+                {
+                    ttaInex++;
+                    timesTrueArray[ttaInex-1] = timesTrue;//number of equal numbers -1 per area
+                    numArray[ttaInex - 1] = num;//stores value of area
+                    timesTrue = 0;
+                }
+            }
+            int mostSameI = 0;
+            int valueInd = 0;
+            for(int i=0;i<ttaInex;i++)
+            {
+
+                mostSameI = Math.Max(mostSameI, timesTrueArray[i]);
+                if(mostSameI==timesTrueArray[i])
+                {
+                    valueInd = i;
+                }
+            }
+
+            //Printing
+            for(int i=0;i<mostSameI+1;i++)
+            {
+                Console.Write(numArray[valueInd]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
