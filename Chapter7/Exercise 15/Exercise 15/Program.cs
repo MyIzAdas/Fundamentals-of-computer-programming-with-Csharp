6cffb015dac63d3204c946eb7acb484ffad7597d
Adas <Adas@URGODS-PC>
2018-04-01 21:08:10 +0100

    ex 15 and 16 solved

---
+++ Chapter7/Exercise 15/Exercise 15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program shows index of letters within the word as in, a-z is 1-26");
+            Console.Write("Please enter your word: ");
+            string word = Console.ReadLine();
+            string[] wArr = { "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","U","W","X","Y","Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "u", "w", "x", "y", "z" };
+            int num = 0;
+            for (int j = 0; j < word.Length; j++)
+            {
+                for (int i = 0; i < wArr.Length; i++)
+                {
+                    if(Convert.ToString(word[j])==wArr[i])
+                    {
+                        if(i>25)
+                        {
+                            num = i - 26;
+                        }
+                        else
+                        {
+                            num = i;
+                        }
+                        Console.Write("{0,3}",num+1);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Chapter7/Exercise 16/Exercise 16/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This programs uses binary search in the sorted array of integer elements.");
+            Console.Write("Please enter the number you want to find in array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = {19,12,55,4,8,10,31,18,26,97 };
+            int lowest = int.MaxValue;
+            int mem = 0;
+            int iLow = 0;
+            bool found = false;
+            for(int i=0;i<arr.Length;i++) //Sorting
+            {
+                for(int j=i;j<arr.Length;j++)
+                {
+                   if(arr[j]<lowest)
+                    {
+                        lowest = arr[j];
+                        iLow = j;
+                    }
+                }
+                mem = arr[i];
+                arr[i] = lowest;
+                arr[iLow] = mem;
+                lowest = int.MaxValue;
+                Console.WriteLine(arr[i]);
+            }
+
+            //Binary search
+            int divisor = arr.Length/2;
+            int count = 0;
+            while (found == false && count<=arr.Length)
+            {
+                if (arr[divisor] == n)
+                {
+                    found = true;
+                }
+                else if (arr[divisor] > n)
+                {
+                    divisor = divisor / 2;
+                }
+                else if(arr.Length/2<n)
+                {
+                    divisor = divisor / 2 + arr.Length/2;
+                }
+                else
+                {
+                    divisor = ((arr.Length / 2) + divisor)/2;
+                }
+                count++;
+            }
+            
+            //Print
+            if(found)
+            {
+                Console.WriteLine(n + " was found.");
+            }
+            else
+            {
+                Console.WriteLine(n + " wansn't found.");
+            }
+        }
+    }
+}
