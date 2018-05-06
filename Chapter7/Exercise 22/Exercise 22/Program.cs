564c0abe158119d4e850d4355a444caacb38dd05
Adas <Adas@URGODS-PC>
2018-05-07 00:17:16 +0100

    adding solutions

---
+++ Chapter7/Exercise 22/Exercise 22/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program finds the longest ascending sequence in array and prints it.");
+            Console.Write("Enter the size of your array: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Now enter values of your array.");
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int maxLength = 1;
+            int length = 0;
+            int biggerCount = 0;
+            int mover = 0;
+            int[] result = new int[n];
+            while (maxLength != 0)
+            {
+                maxLength = -1;
+                for (int i = mover; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (arr[i] <= arr[j])
+                        {
+                            length++;
+                        }
+                    }
+                    if (length > maxLength && biggerCount==0)
+                    {
+                        result[biggerCount] = arr[i];
+                        mover = i+1;
+                        maxLength = length;
+                    }
+                    if (length > maxLength && arr[i]>=result[biggerCount-1])
+                    {
+                        result[biggerCount] = arr[i];
+                        mover = i + 1;
+                        maxLength = length;
+                    }
+                    length = 0;
+                }
+                biggerCount++;
+            }
+            //Print
+            for(int i=0;i<biggerCount;i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Chapter7/Exercise 23/Exercise 23/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_23
+{
+    class Program
+    {
+        static int k;
+        static int[] arr;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program prints all variations of elements from 1 to max value.");
+            Console.Write("Enter the number of values: ");
+            k = int.Parse(Console.ReadLine());
+            Console.Write("Enter the the maximum value: ");
+            int n = int.Parse(Console.ReadLine());
+            arr = new int[k];
+            RecursiveVariatons(0,k, n);
+        }
+        static void RecursiveVariatons(int start,int k,int n)
+        {
+            if (start != k)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    arr[start] = i;
+                    RecursiveVariatons(start + 1, k, n);
+                }
+            }
+            else
+            {
+                Print(k);
+            }
+        }
+        static void Print(int n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
